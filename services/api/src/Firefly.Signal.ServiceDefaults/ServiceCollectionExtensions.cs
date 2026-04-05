1b76b8de4d28edeeb48d8d23109211d2dae4317b
lee mac <lee.wan1204@gmail.com>
2026-04-05 20:04:47 +0100

    add swagger and launchSettings

--- services/api/src/Firefly.Signal.ServiceDefaults/ServiceCollectionExtensions.cs
+++ services/api/src/Firefly.Signal.ServiceDefaults/ServiceCollectionExtensions.cs
@@ -14,6 +14,8 @@ public static class ServiceCollectionExtensions
     public static void AddDefaultOpenApi(this IHostApplicationBuilder builder)
     {
         builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
     }
 
     public static void MapDefaultEndpoints(this WebApplication app)
@@ -23,9 +25,15 @@ public static class ServiceCollectionExtensions
 
     public static void UseDefaultOpenApi(this WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment() || app.Environment.IsEnvironment("Testing"))
         {
             app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/openapi/v1.json", $"{app.Environment.ApplicationName} v1");
+                options.RoutePrefix = "swagger";
+            });
         }
     }
 }
